3708dd15a10f650c296340cb910d12589088beb6
Tom Hacohen <tom@nospam>
2009-10-30 12:38:51 +0200

    added alsa to configure/makefile.am and added  phoneui-utils-sound.*'s content.

--- src/phoneui-utils-sound.c
+++ src/phoneui-utils-sound.c
@@ -1,2 +1,142 @@
+#include <alsa/asoundlib.h>
+
 #include "phoneui-utils-sound.h"
 
+
+#include <stdio.h>
+
+
+
+
+struct SoundControl {
+	const char *name;
+};
+
+static struct SoundControl controls[] = {
+					{"Master Capture Volume"},
+					{"Master Playback Volume"}
+					};
+
+/* The sound cards hardware control */
+static snd_hctl_t *hctl = NULL;
+
+static int
+_phoneui_utils_sound_volume_get_stats(enum SoundDeviceType type, long *_min, long *_max, long *_step, unsigned int *_count)
+{
+	unsigned int count;
+	long min, max, step;
+	const char *ctl_name = controls[type].name;
+	
+	snd_ctl_elem_id_t *id;
+	snd_ctl_elem_id_alloca(&id);
+	snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_MIXER);
+	snd_ctl_elem_id_set_name(id, ctl_name);
+	snd_hctl_elem_t *elem = snd_hctl_find_elem(hctl, id);
+	snd_ctl_elem_type_t element_type;
+	snd_ctl_elem_info_t *info;
+	snd_ctl_elem_info_alloca(&info);
+	
+	if (snd_hctl_elem_info(elem, info) < 0) {
+		return 1;
+	}
+
+	count = snd_ctl_elem_info_get_count(info);
+
+	/* verify type != integer */
+	element_type = snd_ctl_elem_info_get_type(info);
+	min = snd_ctl_elem_info_get_min(info);
+	max = snd_ctl_elem_info_get_max(info);
+	step = snd_ctl_elem_info_get_step(info);
+
+	if (_min)
+		*_min = min;
+	if (_max)
+		*_max = max;
+	if (_step)
+		*_step = step;
+	if (_count)
+		*_count = count;
+		
+	return 0;
+}
+
+int
+phoneui_utils_sound_volume_get(enum SoundDeviceType type)
+{
+	long value, min, max;
+	unsigned int i,count;
+	const char *ctl_name = controls[type].name;
+	
+	snd_ctl_elem_value_t *control;
+	snd_ctl_elem_value_alloca(&control);
+	snd_ctl_elem_id_t *id;
+	snd_ctl_elem_id_alloca(&id);
+	snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_MIXER);
+	snd_ctl_elem_id_set_name(id, ctl_name);
+	snd_hctl_elem_t *elem = snd_hctl_find_elem(hctl, id);
+	
+	if (_phoneui_utils_sound_volume_get_stats(type, &min, &max, NULL, &count))
+		return -1;
+
+	if (snd_hctl_elem_read(elem, control) < 0) {
+		return 1;
+	}
+	value = 0;
+	/* FIXME: possible long overflow */
+	for (i = 0 ; i < count ; i++) {
+		value += snd_ctl_elem_value_get_integer(control, i);
+	}
+	value /= count;
+	
+	return ((double) (value - min) / (max - min)) * 100.0;
+}
+
+int
+phoneui_utils_sound_volume_set(enum SoundDeviceType type, int percent)
+{
+	int err;
+	long new_value;
+	unsigned int i, count;
+	long min, max;
+	const char *ctl_name = controls[type].name;
+	/*FIXME: verify it's writeable and of the correct element type */
+	snd_ctl_elem_id_t *id;
+	snd_ctl_elem_id_alloca(&id);
+	snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_MIXER);
+	snd_ctl_elem_id_set_name(id, ctl_name);
+	snd_hctl_elem_t *elem = snd_hctl_find_elem(hctl, id);
+	snd_ctl_elem_value_t *control;
+	snd_ctl_elem_value_alloca(&control);
+	snd_ctl_elem_value_set_id(control, id);
+	if (_phoneui_utils_sound_volume_get_stats(type, &min, &max, NULL, &count))
+		return -1;
+	new_value = ((max - min) * percent) / 100;
+	for (i = 0 ; i < count ; i++) {		
+		snd_ctl_elem_value_set_integer(control, i, new_value);
+	}
+
+	err = snd_hctl_elem_write(elem, control);
+
+	return 0;
+}
+
+int
+phoneui_utils_sound_init(const char *device_name)
+{
+	int err;
+	//err = snd_hctl_open(&hctl, "hw:0", 0);
+	if (hctl) {
+		snd_hctl_close(hctl);
+	}
+	err = snd_hctl_open(&hctl, device_name, 0);
+	err = snd_hctl_load(hctl);
+	return err;
+}
+
+int
+phoneui_utils_sound_deinit()
+{
+	snd_hctl_close(hctl);
+	hctl = NULL;
+	return 0;
+}
--- src/phoneui-utils-sound.h
+++ src/phoneui-utils-sound.h
@@ -1,4 +1,14 @@
 #ifndef _PHONEUI_UTILS_SOUND_H
 #define _PHONEUI_UTILS_SOUND_H
+enum SoundDeviceType {
+	DEVICE_MICROPHONE = 0,
+	DEVICE_SPEAKER
+};
+int phoneui_utils_sound_volume_get(enum SoundDeviceType type);
 
+int phoneui_utils_sound_volume_set(enum SoundDeviceType type, int percent);
+
+int phoneui_utils_sound_init(const char *device_name);
+
+int phoneui_utils_sound_deinit();
 #endif
