e8ef0e84c9009c9284c4f25fd9c332cbaba5f79a
Lukas Märdian <lukasmaerdian@gmail.com>
2009-12-12 17:35:02 +0100

    fix

--- src/phoneui-info.c
+++ src/phoneui-info.c
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-#include <frameworkd-glib/frameworkd-glib-dbus.h>
-#include <frameworkd-glib/ogsmd/frameworkd-glib-ogsmd-call.h>
-#include <frameworkd-glib/ogsmd/frameworkd-glib-ogsmd-network.h>
-#include <frameworkd-glib/odeviced/frameworkd-glib-odeviced-powersupply.h>
-#include <frameworkd-glib/ousaged/frameworkd-glib-ousaged.h>
-#include <frameworkd-glib/opimd/frameworkd-glib-opimd-calls.h>
-#include <frameworkd-glib/opimd/frameworkd-glib-opimd-messages.h>
-#include <frameworkd-glib/opreferencesd/frameworkd-glib-opreferencesd-preferences.h>
-
->>>>>>> 77b0255ad2adf485a7347d9925646c06ebca868e
-=======
 #include <glib.h>
 #include <glib-object.h>
 
->>>>>>> 2d96e28ed763de21b5d7058c2334db66564d42ea
 #include "phoneui.h"
 
 static void _missed_calls_handler(const int amount);
@@ -57,18 +41,9 @@ int
 phoneui_info_init()
 {
 
-<<<<<<< HEAD
 	g_debug("phoneui_info_init: connecting to libframeworkd-glib");
 	// TODO: feed the idle screen with faked signals to get initial data
 
-<<<<<<< HEAD
-	g_debug("phoneui_info_init: done");
-=======
-	frameworkd_handler_connect(fw);
-
->>>>>>> 77b0255ad2adf485a7347d9925646c06ebca868e
-=======
->>>>>>> 2d96e28ed763de21b5d7058c2334db66564d42ea
 	return 0;
 }
 
@@ -77,11 +52,8 @@ phoneui_info_trigger()
 {
 	/* manually feed initial data to the idle screen... further
 	 * updates will be handled by the signal handlers registered above */
-<<<<<<< HEAD
-=======
 	g_debug("%s", __FUNCTION__);
 #if 0
->>>>>>> 2d96e28ed763de21b5d7058c2334db66564d42ea
 	opimd_calls_get_new_missed_calls(_missed_calls_callback, NULL);
 	opimd_messages_get_unread_messages(_unread_messages_callback, NULL);
 	//TODO unfinished tasks
@@ -91,10 +63,7 @@ phoneui_info_trigger()
 	odeviced_power_supply_get_capacity(_get_capacity_callback, NULL);
 	ogsmd_network_get_status(_get_network_status_callback, NULL);
 	ogsmd_network_get_signal_strength(_get_signal_strength_callback, NULL);
-<<<<<<< HEAD
-=======
 #endif
->>>>>>> 2d96e28ed763de21b5d7058c2334db66564d42ea
 }
 
 /* --- signal handlers --- */
@@ -158,9 +127,6 @@ static void _idle_notifier_handler(const int state)
 }
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 /* callbacks for initial feeding of data */
 
 static void _missed_calls_callback(GError *error,
@@ -300,7 +266,6 @@ static void _handle_network_status(GHashTable *properties)
 	g_hash_table_destroy(properties);
 }
 
->>>>>>> 2d96e28ed763de21b5d7058c2334db66564d42ea
 static void hack2();
 static void hack1()
 {
@@ -328,146 +293,5 @@ _get_signal_strength_callback(NULL, 0, NULL);
 static void hack2()
 {
 	hack1();
-=======
-/* callbacks for initial feeding of data */
-
-static void _missed_calls_callback(GError *error,
-		const int amount, gpointer userdata)
-{
-	(void)userdata;
-
-	if (error) {
-		g_message("_missed_calls_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	phoneui_idle_screen_update_missed_calls(amount);
-}
-
-static void _unread_messages_callback(GError *error,
-		const int amount, gpointer userdata)
-{
-	(void)userdata;
-
-	if (error) {
-		g_message("_unread_messages_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	phoneui_idle_screen_update_unread_messages(amount);
-}
-
-static void _list_resources_callback(GError *error,
-		char **resources, gpointer userdata)
-{
-	(void)userdata;
-
-	if (error) {
-		g_message("_list_resources_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	if (resources) {
-		int i = 0;
-		while (resources[i] != NULL) {
-			ousaged_get_resource_state(resources[i],
-					_resource_state_callback,
-					resources[i]);
-			i++;
-		}
-	}
-}
-
-static void _resource_state_callback(GError *error,
-		gboolean state, gpointer userdata)
-{
-	if (error) {
-		g_message("_resource_state_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	phoneui_idle_screen_update_resource((char *)userdata, state);
-	// TODO: free(userdata);
-}
-
-static void _get_profile_callback(GError *error,
-		const char *profile, gpointer userdata)
-{
-	(void)userdata;
-
-	if (error) {
-		g_message("_get_profile_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	phoneui_idle_screen_update_profile(profile);
-}
-
-static void _get_capacity_callback(GError *error,
-		const int energy, gpointer userdata)
-{
-	(void)userdata;
-
-	if (error) {
-		g_message("_get_capacity_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	phoneui_idle_screen_update_power(energy);
-}
-
-static void _get_network_status_callback(GError *error,
-		GHashTable *properties, gpointer userdata)
-{
-	(void)userdata;
-
-	if (error) {
-		g_message("_get_network_status_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	_handle_network_status(properties);
-}
-
-static void _get_signal_strength_callback(GError *error,
-		const int signal, gpointer userdata)
-{
-	(void)userdata;
-
-	if (error) {
-		g_message("_get_signal_strength_callback: error %d: %s",
-				error->code, error->message);
-		return;
-	}
-	phoneui_idle_screen_update_signal_strength(signal);
-}
-
-//static void _get_alarm_callback(GError *error,
-//		const int time, gpointer userdata)
-//{
-//	(void)userdata;
-//
-//	if (error) {
-//		g_message("_get_alarm_callback: error %d: %s",
-//				error->code, error->message);
-//		return;
-//	}
-//	phoneui_idle_screen_update_alarm(time);
-//}
-
-static void _handle_network_status(GHashTable *properties)
-{
-	g_debug("_handle_network_status");
-	if (properties == NULL) {
-		g_message("_handle_network_status: no properties!");
-		return;
-	}
-	GValue *v = g_hash_table_lookup(properties, "provider");
-	if (v) {
-		g_debug("provider is '%s'", g_value_get_string(v));
-		phoneui_idle_screen_update_provider(g_value_get_string(v));
-	}
-	g_hash_table_destroy(properties);
->>>>>>> 77b0255ad2adf485a7347d9925646c06ebca868e
 }
 
