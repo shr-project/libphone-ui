42f8f92b9151657de493737c6fac553470cc4bb8
Klaus Kurzmann <mok@fluxnetz.de>
2010-02-27 08:36:44 +0100

    phoneui-info: add callback registration facillity for more signals this adds: - profile changes - capacity changes - missed calls - unread messages - resource changes - network status - signal strength
    all compile tested only :P
    
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/phoneui-info.c
+++ src/phoneui-info.c
@@ -11,11 +11,20 @@
 #include <frameworkd-glib/opimd/frameworkd-glib-opimd-messages.h>
 #include <frameworkd-glib/opreferencesd/frameworkd-glib-opreferencesd-preferences.h>
 
-static GList *callbacks_contact_changes = NULL;
-static GList *callbacks_pdp_network_status = NULL;
-
 #include "phoneui.h"
 #include "phoneui-info.h"
+
+static GList *callbacks_contact_changes = NULL;
+static GList *callbacks_message_changes = NULL;
+static GList *callbacks_pdp_network_status = NULL;
+static GList *callbacks_profile_changes = NULL;
+static GList *callbacks_capacity_changes = NULL;
+static GList *callbacks_missed_calls = NULL;
+static GList *callbacks_unread_messages = NULL;
+static GList *callbacks_resource_changes = NULL;
+static GList *callbacks_network_status = NULL;
+static GList *callbacks_signal_strength = NULL;
+
 
 static void _missed_calls_handler(int amount);
 static void _unread_messages_handler(int amount);
@@ -32,6 +41,9 @@ static void _idle_notifier_handler(int state);
 static void _pim_contact_new_handler(const char *path);
 static void _pim_contact_updated_handler(const char *path, GHashTable *content);
 static void _pim_contact_deleted_handler(const char *path);
+static void _pim_message_new_handler(const char *path);
+static void _pim_message_updated_handler(const char *path, GHashTable *content);
+static void _pim_message_deleted_handler(const char *path);
 
 static void _missed_calls_callback(GError *error, int amount, gpointer userdata);
 static void _unread_messages_callback(GError *error, int amount, gpointer userdata);
@@ -46,7 +58,11 @@ static void _get_signal_strength_callback(GError *error, int signal, gpointer us
 
 static void _handle_network_status(GHashTable *properties);
 
-static void _execute_contact_callbacks(const char *path, enum PhoneuiInfoChangeType type);
+static void _execute_pim_changed_callbacks(GList *cbs, const char *path, enum PhoneuiInfoChangeType type);
+static void _execute_int_callbacks(GList *cbs, int value);
+static void _execute_charp_callbacks(GList *cbs, const char *value);
+static void _execute_hashtable_callbacks(GList *cbs, GHashTable *properties);
+static void _execute_resource_callbacks(GList *cbs, const char *resource, gboolean state, GHashTable *properties);
 
 int
 phoneui_info_init()
@@ -67,6 +83,9 @@ phoneui_info_init()
 	fw->pimNewContact = _pim_contact_new_handler;
 	fw->pimUpdatedContact = _pim_contact_updated_handler;
 	fw->pimDeletedContact = _pim_contact_deleted_handler;
+	fw->pimNewMessage = _pim_message_new_handler;
+	fw->pimUpdatedMessage = _pim_message_updated_handler;
+	fw->pimDeletedMessage = _pim_message_deleted_handler;
 
 	frameworkd_handler_connect(fw);
 
@@ -89,10 +108,27 @@ phoneui_info_trigger()
 	ogsmd_network_get_signal_strength(_get_signal_strength_callback, NULL);
 }
 
-struct _cb_contact_changes_pack {
+struct _cb_pim_changes_pack {
 	void (*callback)(void *, const char *, enum PhoneuiInfoChangeType);
 	void *data;
 };
+struct _cb_hashtable_pack {
+	void (*callback)(void *, GHashTable *);
+	void *data;
+};
+struct _cb_int_pack {
+	void (*callback)(void *, int);
+	void *data;
+};
+struct _cb_charp_pack {
+	void (*callback)(void *, const char *);
+	void *data;
+};
+struct _cb_resource_changes_pack {
+	void (*callback)(void *, const char*, gboolean, GHashTable *);
+	void *data;
+};
+
 
 void
 phoneui_info_register_contact_changes(void (*callback)(void *, const char*,
@@ -104,8 +140,8 @@ phoneui_info_register_contact_changes(void (*callback)(void *, const char*,
 		g_debug("Not registering an empty callback - fix your code");
 		return;
 	}
-	struct _cb_contact_changes_pack *pack =
-			malloc(sizeof(struct _cb_contact_changes_pack));
+	struct _cb_pim_changes_pack *pack =
+			malloc(sizeof(struct _cb_pim_changes_pack));
 	if (!pack) {
 		g_warning("Failed allocating callback pack - not registering");
 		return;
@@ -124,13 +160,9 @@ phoneui_info_register_contact_changes(void (*callback)(void *, const char*,
 	}
 }
 
-struct _cb_pdp_network_status_pack {
-	void (*callback)(void *, GHashTable *);
-	void *data;
-};
-
-void phoneui_info_register_pdp_network_status(void (*callback)(void *,
-					      GHashTable *), void *data)
+void
+phoneui_info_register_message_changes(void (*callback)(void *, const char*,
+				enum PhoneuiInfoChangeType), void *data)
 {
 	GList *l;
 
@@ -138,10 +170,40 @@ void phoneui_info_register_pdp_network_status(void (*callback)(void *,
 		g_debug("Not registering an empty callback - fix your code");
 		return;
 	}
-	struct _cb_pdp_network_status_pack *pack =
-			malloc(sizeof(struct _cb_pdp_network_status_pack));
+	struct _cb_pim_changes_pack *pack =
+			malloc(sizeof(struct _cb_pim_changes_pack));
 	if (!pack) {
 		g_warning("Failed allocating callback pack - not registering");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_message_changes, pack);
+	if (!l) {
+		g_warning("Failed to register callback for message changes");
+	}
+	else {
+		if (!callbacks_message_changes) {
+			callbacks_message_changes = l;
+			g_debug("Registered a callback for message changes");
+		}
+	}
+}
+
+
+void phoneui_info_register_pdp_network_status(void (*callback)(void *,
+					      GHashTable *), void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (pdp network status)");
+		return;
+	}
+	struct _cb_hashtable_pack *pack =
+			malloc(sizeof(struct _cb_hashtable_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (pdp network status)");
 		return;
 	}
 	pack->callback = callback;
@@ -152,23 +214,240 @@ void phoneui_info_register_pdp_network_status(void (*callback)(void *,
 	}
 	else {
 		if (!callbacks_pdp_network_status) {
-			callbacks_contact_changes = l;
+			callbacks_pdp_network_status = l;
 			g_debug("Registered a callback for pdp network status");
 		}
 	}
 }
+
+void
+phoneui_info_register_profile_changes(void (*callback)(void *, const char *),
+				      void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (profile changes)");
+		return;
+	}
+	struct _cb_charp_pack *pack =
+			malloc(sizeof(struct _cb_charp_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (profile changes)");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_profile_changes, pack);
+	if (!l) {
+		g_warning("Failed to register callback for profile changes");
+	}
+	else {
+		if (!callbacks_profile_changes) {
+			callbacks_profile_changes = l;
+			g_debug("Registered a callback for profile changes");
+		}
+	}
+}
+
+void
+phoneui_info_register_capacity_changes(void (*callback)(void *, int),
+				       void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (capacity changes)");
+		return;
+	}
+	struct _cb_int_pack *pack =
+			malloc(sizeof(struct _cb_int_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (capacity changes)");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_capacity_changes, pack);
+	if (!l) {
+		g_warning("Failed to register callback for capacity changes");
+	}
+	else {
+		if (!callbacks_capacity_changes) {
+			callbacks_capacity_changes = l;
+			g_debug("Registered a callback for capacity changes");
+		}
+	}
+}
+
+void
+phoneui_info_register_missed_calls(void (*callback)(void *, int),
+				   void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (missed calls)");
+		return;
+	}
+	struct _cb_int_pack *pack =
+			malloc(sizeof(struct _cb_int_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (missed calls)");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_missed_calls, pack);
+	if (!l) {
+		g_warning("Failed to register callback for missed calls");
+	}
+	else {
+		if (!callbacks_missed_calls) {
+			callbacks_missed_calls = l;
+			g_debug("Registered a callback for missed calls");
+		}
+	}
+}
+
+void
+phoneui_info_register_unread_messages(void (*callback)(void *, int),
+				      void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (unread messages)");
+		return;
+	}
+	struct _cb_int_pack *pack =
+			malloc(sizeof(struct _cb_int_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (unread messages)");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_unread_messages, pack);
+	if (!l) {
+		g_warning("Failed to register callback for unread messages");
+	}
+	else {
+		if (!callbacks_unread_messages) {
+			callbacks_unread_messages = l;
+			g_debug("Registered a callback for unread messages");
+		}
+	}
+}
+
+void
+phoneui_info_register_resource_changes(void (*callback)(void *, const char *,
+						   gboolean, GHashTable *),
+				       void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (resource changes)");
+		return;
+	}
+	struct _cb_resource_changes_pack *pack =
+			malloc(sizeof(struct _cb_resource_changes_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (resource changes)");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_resource_changes, pack);
+	if (!l) {
+		g_warning("Failed to register callback for resource changes");
+	}
+	else {
+		if (!callbacks_resource_changes) {
+			callbacks_resource_changes = l;
+			g_debug("Registered a callback for resource changes");
+		}
+	}
+}
+
+void
+phoneui_info_register_network_status(void (*callback)(void *, GHashTable *),
+				     void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (network status)");
+		return;
+	}
+	struct _cb_hashtable_pack *pack =
+			malloc(sizeof(struct _cb_hashtable_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (network status)");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_network_status, pack);
+	if (!l) {
+		g_warning("Failed to register callback for network status");
+	}
+	else {
+		if (!callbacks_network_status) {
+			callbacks_network_status = l;
+			g_debug("Registered a callback for network status");
+		}
+	}
+}
+
+void
+phoneui_info_register_signal_strength(void (*callback)(void *, int),
+				      void *data)
+{
+	GList *l;
+
+	if (!callback) {
+		g_debug("Not registering an empty callback (signal strength)");
+		return;
+	}
+	struct _cb_int_pack *pack =
+			malloc(sizeof(struct _cb_int_pack));
+	if (!pack) {
+		g_warning("Failed allocating callback pack (signal strength)");
+		return;
+	}
+	pack->callback = callback;
+	pack->data = data;
+	l = g_list_append(callbacks_signal_strength, pack);
+	if (!l) {
+		g_warning("Failed to register callback for signal strength");
+	}
+	else {
+		if (!callbacks_signal_strength) {
+			callbacks_signal_strength = l;
+			g_debug("Registered a callback for signal strength");
+		}
+	}
+}
+
+
 
 /* --- signal handlers --- */
 
 static void _missed_calls_handler(int amount)
 {
 	g_debug("_missed_calls_handler: %d missed calls", amount);
+	_execute_int_callbacks(callbacks_missed_calls, amount);
+	// FIXME: remove when idle screen is using callbacks
 	phoneui_idle_screen_update_missed_calls(amount);
 }
 
 static void _unread_messages_handler(const int amount)
 {
 	g_debug("_unread_messages_handler: %d unread messages", amount);
+	_execute_int_callbacks(callbacks_unread_messages, amount);
+	// FIXME: remove when idle screen is using callbacks
 	phoneui_idle_screen_update_unread_messages(amount);
 }
 
@@ -184,6 +463,9 @@ static void _resource_changed_handler(const char *resource,
 	(void)properties;
 	g_debug("_resource_changed_handler: %s is now %s",
 			resource, state ? "enabled" : "disabled");
+	_execute_resource_callbacks(callbacks_resource_changes, resource,
+				    state, properties);
+	// FIXME: remove when idle screen is using callbacks
 	phoneui_idle_screen_update_resource(resource, state);
 }
 
@@ -217,26 +499,14 @@ static void _call_status_handler(int callid, int state,
 
 static void _pdp_network_status_handler(GHashTable *status)
 {
-	GList *cb;
-
-	if (!callbacks_pdp_network_status) {
-		g_debug("No callbacks registered for pdp network status");
-		return;
-	}
-
-	g_debug("Running all pdp network status callbacks");
-	for (cb = g_list_first(callbacks_pdp_network_status); cb;
-					cb = g_list_next(cb)) {
-		struct _cb_pdp_network_status_pack *pack =
-			(struct _cb_pdp_network_status_pack *)cb->data;
-		pack->callback(pack->data, status);
-	}
+	_execute_hashtable_callbacks(callbacks_pdp_network_status, status);
 }
 
 static void _profile_changed_handler(const char *profile)
 {
 	g_debug("_profile_changed_handler: active profile is %s", profile);
 	phoneui_idle_screen_update_profile(profile);
+	_execute_charp_callbacks(callbacks_profile_changes, profile);
 }
 
 //static void _alarm_changed_handler(const int time)
@@ -248,6 +518,8 @@ static void _profile_changed_handler(const char *profile)
 static void _capacity_changed_handler(int energy)
 {
 	g_debug("_capacity_changed_handler: capacity is %d", energy);
+	_execute_int_callbacks(callbacks_capacity_changes, energy);
+	// FIXME: remove when idle screen uses callbacks
 	phoneui_idle_screen_update_power(energy);
 }
 
@@ -259,6 +531,8 @@ static void _network_status_handler(GHashTable *properties)
 static void _signal_strength_handler(int signal)
 {
 	g_debug("_signal_strength_handler: %d", signal);
+	_execute_int_callbacks(callbacks_signal_strength, signal);
+	// FIXME: remove when idle screen uses callbacks
 	phoneui_idle_screen_update_signal_strength(signal);
 }
 
@@ -270,21 +544,47 @@ static void _idle_notifier_handler(int state)
 static void _pim_contact_new_handler(const char *path)
 {
 	g_debug("New contact %s got added", path);
-	_execute_contact_callbacks(path, PHONEUI_INFO_CHANGE_NEW);
+	_execute_pim_changed_callbacks(callbacks_contact_changes,
+				       path, PHONEUI_INFO_CHANGE_NEW);
 }
 
 static void _pim_contact_updated_handler(const char *path, GHashTable *content)
 {
 	(void)content;
 	g_debug("Contact %s got updated", path);
-	_execute_contact_callbacks(path, PHONEUI_INFO_CHANGE_UPDATE);
+	_execute_pim_changed_callbacks(callbacks_contact_changes,
+				       path, PHONEUI_INFO_CHANGE_UPDATE);
 }
 
 static void _pim_contact_deleted_handler(const char *path)
 {
 	g_debug("Contact %s got deleted", path);
-	_execute_contact_callbacks(path, PHONEUI_INFO_CHANGE_DELETE);
+	_execute_pim_changed_callbacks(callbacks_contact_changes,
+				       path, PHONEUI_INFO_CHANGE_DELETE);
 }
+
+static void _pim_message_new_handler(const char *path)
+{
+	g_debug("New message %s got added", path);
+	_execute_pim_changed_callbacks(callbacks_message_changes,
+				       path, PHONEUI_INFO_CHANGE_NEW);
+}
+
+static void _pim_message_updated_handler(const char *path, GHashTable *content)
+{
+	(void)content;
+	g_debug("Message %s got updated", path);
+	_execute_pim_changed_callbacks(callbacks_message_changes,
+				       path, PHONEUI_INFO_CHANGE_UPDATE);
+}
+
+static void _pim_message_deleted_handler(const char *path)
+{
+	g_debug("Message %s got deleted", path);
+	_execute_pim_changed_callbacks(callbacks_message_changes,
+				       path, PHONEUI_INFO_CHANGE_DELETE);
+}
+
 
 /* callbacks for initial feeding of data */
 
@@ -432,21 +732,80 @@ static void _handle_network_status(GHashTable *properties)
 	g_hash_table_destroy(properties);
 }
 
-static void _execute_contact_callbacks(const char *path,
+static void _execute_pim_changed_callbacks(GList *cbs, const char *path,
 				       enum PhoneuiInfoChangeType type)
 {
 	GList *cb;
 
-	if (!callbacks_contact_changes) {
-		g_debug("No callbacks registered for contact changes");
+	if (!cbs) {
+		g_debug("No callbacks registered");
 		return;
 	}
 
-	g_debug("Running all contact changed callbacks");
-	for (cb = g_list_first(callbacks_contact_changes); cb;
-					cb = g_list_next(cb)) {
-		struct _cb_contact_changes_pack *pack =
-			(struct _cb_contact_changes_pack *)cb->data;
+	g_debug("Running all callbacks");
+	for (cb = g_list_first(cbs); cb; cb = g_list_next(cb)) {
+		struct _cb_pim_changes_pack *pack =
+			(struct _cb_pim_changes_pack *)cb->data;
 		pack->callback(pack->data, path, type);
 	}
 }
+
+static void
+_execute_int_callbacks(GList *cbs, int value)
+{
+	GList *cb;
+
+	if (!cbs)
+		return;
+
+	for (cb = g_list_first(cbs); cb; cb = g_list_next(cb)) {
+		struct _cb_int_pack *pack = (struct _cb_int_pack *)cb->data;
+		pack->callback(pack->data, value);
+	}
+}
+
+static void
+_execute_hashtable_callbacks(GList *cbs, GHashTable *properties)
+{
+	GList *cb;
+
+	if (!cbs)
+		return;
+
+	for (cb = g_list_first(cbs); cb; cb = g_list_next(cb)) {
+		struct _cb_hashtable_pack *pack =
+				(struct _cb_hashtable_pack *)cb->data;
+		pack->callback(pack->data, properties);
+	}
+}
+
+static void
+_execute_charp_callbacks(GList *cbs, const char *value)
+{
+	GList *cb;
+
+	if (!cbs)
+		return;
+
+	for (cb = g_list_first(cbs); cb; cb = g_list_next(cb)) {
+		struct _cb_charp_pack *pack = (struct _cb_charp_pack *)cb->data;
+		pack->callback(pack->data, value);
+	}
+}
+
+static void
+_execute_resource_callbacks(GList *cbs, const char *resource, gboolean state,
+			    GHashTable *properties)
+{
+	GList *cb;
+
+	if (!cbs)
+		return;
+
+	for (cb = g_list_first(cbs); cb; cb = g_list_next(cb)) {
+		struct _cb_resource_changes_pack *pack =
+				(struct _cb_resource_changes_pack *)cb->data;
+		pack->callback(pack->data, resource, state, properties);
+	}
+}
+
--- src/phoneui-info.h
+++ src/phoneui-info.h
@@ -10,9 +10,20 @@ enum PhoneuiInfoChangeType {
 
 int phoneui_info_init();
 void phoneui_info_trigger();
+
 void phoneui_info_register_contact_changes(void (*_cb)(void *, const char *,
-				enum PhoneuiInfoChangeType), void *data);
-void phoneui_info_register_pdp_network_status(void (*_cb)(void *, GHashTable *),
-					      void *data);
+						       enum PhoneuiInfoChangeType), void *data);
+void phoneui_info_register_message_changes(void (*_cb)(void *, const char *,
+						       enum PhoneuiInfoChangeType), void *data);
+void phoneui_info_register_pdp_network_status(void (*_cb)(void *, GHashTable *), void *data);
+
+void phoneui_info_register_profile_changes(void (*_cb)(void *, const char *), void *data);
+void phoneui_info_register_capacity_changes(void (*_cb)(void *, int), void *data);
+void phoneui_info_register_missed_calls(void (*_cb)(void *, int), void *data);
+void phoneui_info_register_unread_messages(void (*_cb)(void *, int), void *data);
+void phoneui_info_register_resource_changes(void (*_cb)(void *, const char *, gboolean, GHashTable *), void *data);
+void phoneui_info_register_network_status(void (*_cb)(void *, GHashTable *), void *data);
+void phoneui_info_register_signal_strength(void (*_cb)(void *, int), void *data);
+
 #endif
 
