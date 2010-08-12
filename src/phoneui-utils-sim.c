42cefe7d0b479329b99b1f229dcfcd194d55e0f8
Klaus Kurzmann <mok@fluxnetz.de>
2010-08-12 17:01:01 +0200

    fix authors according to git blame
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/phoneui-utils-sim.c
+++ src/phoneui-utils-sim.c
@@ -1,14 +1,13 @@
 /*
  *  Copyright (C) 2009, 2010
  *      Authors (alphabetical) :
- * 		Tom "TAsn" Hacohen <tom@stosb.com>
- * 		Klaus 'mrmoku' Kurzmann <mok@fluxnetz.de>
+ *		Klaus 'mrmoku' Kurzmann <mok@fluxnetz.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
